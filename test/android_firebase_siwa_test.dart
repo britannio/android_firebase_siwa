b133721a777530dc9fc947091873957b0453a165
Britannio <britanniojarrett@gmail.com>
2020-07-15 22:13:25 +0100

    fix example and test not compiling

--- test/android_firebase_siwa_test.dart
+++ test/android_firebase_siwa_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:android_firebase_siwa/android_firebase_siwa.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('android_firebase_siwa');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await AndroidFirebaseSiwa.instance.platformVersion, '42');
   });
 }
